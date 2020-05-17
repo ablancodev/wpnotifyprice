8469b80a05970249f2d8270f6866c72d2aad00e9
eggemplo <eggemplo@gmail.com>
2020-05-17 17:31:18 +0200

    madamos los emails como bcc

--- core/class-wpnotifyprice.php
+++ core/class-wpnotifyprice.php
@@ -9,7 +9,17 @@ class WPNotifyPrice {
     }
     
     public static function send_notifications( $product_id ) {
-        error_log( "Enviamos notificaciones del producto: " . $product_id );
+        global $wpdb;
+        
+        $leads = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM " . $wpdb->base_prefix . "wpnotifyprice_lead WHERE product_id = %d", intval( $product_id ) ) );
+        if ( $leads && ( sizeof( $leads ) > 0 ) ) {
+            $header = array('Content-Type: text/html; charset=UTF-8');
+            foreach ( $leads as $lead ) {
+                $header[] = 'BCC: ' . $lead->email;
+            }
+            // Mandamos el email
+            @wp_mail( '', 'Bajada de precio', 'Tenemos buenas noticias.<br>El producto ha bajado de precio.', $header );
+        }
     }
 }
 ?>
